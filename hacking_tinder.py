8a2f330fa50a10d60189dc9672fdd43d2ff2fac2
jimako <nsplat@gmail.com>
2017-11-18 15:03:05 +0900

    Modified hacking_tinder.py to use it with pynder.

--- hacking_tinder.py
+++ hacking_tinder.py
@@ -1,116 +1,52 @@
 # -*- coding:utf-8 -*-
-import json
+import io
+import sys
+import pynder
 import asyncio
-from scapy.all import *
-import requests
-from munch import Munch
-
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36"
-RECS_URL = "https://api.gotinder.com/recs/core?locale=ja"
-
-
-class SuiffingPacket:
-    loop = asyncio.get_event_loop()
-    buff_size = 2048
-
-    def __init__(self):
-        self.conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-        self.sniff()
-        self.loop.run_forever()
-
-    def sniff(self):
-        # pkt = self.conn.recv(self.buff_size)
-        # hexvalue = binascii.hexlify(pkt).decode()
-        # print([hexvalue[i:i + 2] for i in range(0, len(hexvalue), 2)])
-        sniff(filter="tcp and port 80", prn=self.packet_callback, store=0)
-        self.loop.call_later(0.1, self.sniff)
-
-    def packet_callback(self, packet):
-        http_packet=str(packet)
-        if http_packet.find('GET'):
-            return self.get_print(packet)
-
-    def get_print(self, packet1):
-        ret = "*****GET PACKET*****\n"
-        ret += "\n".join(packet1.sprintf("{Raw:%Raw.load%}\n").split(r"\r\n"))
-        ret += "********************\n"
-        return ret
 
 
 class HackingTinder:
     headers = None
     loop = asyncio.get_event_loop()
-    counter = 0
+    count = 0
 
-    def __init__(self, token):
-        self.headers = {
-            "User-Agent": USER_AGENT,
-            "X-Auth-Token": token
-        }
+    def __init__(self, facebook_auth_token):
+        self.session = pynder.Session(facebook_token=facebook_auth_token)
+        self.session.matches() # get users you have already been matched with
+
+        # self.session.update_location(LAT, LON)  # updates latitude and longitude for your profile
+        # self.session.profile  # your profile. If you update its attributes they will be updated on Tinder.
+
+        self.users = self.session.nearby_users()  # returns a iterable of users nearby
+
         self.logic()
         self.loop.run_forever()
 
     def logic(self):
-        persons = self.get_recs()
+        print("Sending the heartbeat.")
+        self.logic_impl()
+        self.count += 1
+        self.loop.call_later(1, self.logic)
 
-        for person in persons:
-            # self.print_person_info(person)
-            like_json = requests.get(self.make_like_url(person._id), headers=self.headers)
-            res = json.loads((like_json.text))
-            if "status" in res and res["status"] == 401:
-                raise RuntimeError(
-                    "Something is wrong. It might be because make_like_url() is failed with the expire X-Auth-Token."
-                )
-            print("#" + str(self.counter) + ": SENT LIKE TO HER (" + person._id + ") : " + like_json.text)
-            self.counter += 1
-            time.sleep(0.1)
-
-        self.loop.call_soon(self.logic)
-
-    def get_recs(self):
-        result = []
+    def logic_impl(self):
         try:
-            recs_json = requests.get(RECS_URL, headers=self.headers)
-            recs = Munch(json.loads(recs_json.text))
-            for rec in recs.results:
-                result.append(Munch(rec))
-            return result
-        except:
-            raise RuntimeError(
-                "Something is wrong. It might be because make_like_url() is failed with the expire X-Auth-Token."
-            )
+            user = self.users.__next__()
+            print("#" + str(self.count) + ": ===================================")
+            print(user.name)
+            print(user.bio)
+            print(user.photos)
+            print(user.instagram_username)
+            user.like()
+        except Exception as e:
+            print("Some error was caught : " + str(e))
 
-    def print_person_info(self, person):
-        """
-        :param person: Munch
-        :return: void
-        """
-        try:
-            print(person)
-        except:
-            pass
-        name = person.name
-        sys.stdout.buffer.write("name:" + name)
 
-        bio = person.bio.encode('utf_8')
-        sys.stdout.buffer.write("bio:" + bio)
+def init():
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-        birth_date = person.birth_date
-        sys.stdout.write("birth_date:" + birth_date)
-
-        distance_mi = str(person.distance_mi)
-        sys.stdout.write("distance_mi:" + str(distance_mi))
-
-        photos = person.photos
-        sys.stdout.write("photos:" + photos)
-
-    def make_like_url(self, id):
-        return "https://api.gotinder.com/like/" + id + "?locale=ja"
 
 if __name__ == '__main__':
-    SuiffingPacket()
-    # try:
-    #     token = str(sys.argv[1])
-    #     ht = HackingTinder(token)
-    # except IndexError:
-    #     print("X-Auth-Token IS EMPTY.")
+    # init()
+    facebook_auth_token = sys.argv[1]
+    print("Starting the authentification.")
+    ht = HackingTinder(facebook_auth_token)
---
+++ hacking_tinder.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+. ./setenv.sh
+python3 hacking_tinder.py ${FACEBOOK_AUTH_TOKEN}
